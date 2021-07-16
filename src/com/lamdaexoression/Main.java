c2af6ce2023078435907f655cd75654f1c9ec0e9
mithun <mtnsaha@gmail.com>
2021-07-16 14:18:36 +0530

    new commit

--- src/com/lamdaexoression/Main.java
+++ src/com/lamdaexoression/Main.java
@@ -1,6 +1,6 @@
 package com.lamdaexoression;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 
 public class Main  {
     public static void main(String [] args){
--- src/test/Calculator.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class Calculator {
-    public int multiply(int a, int b)
-    {
-        return a*b;
-    }
-}
